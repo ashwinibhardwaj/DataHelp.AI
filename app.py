9948e29ee2cc58ffbdedc7fb7c23db0961eaa6d6
Ashwini Bhardwaj <114825460+ashwinibhardwaj@users.noreply.github.com>
2025-06-18 19:34:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -227,5 +227,6 @@ def reset():
     return redirect(url_for("index")) 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
